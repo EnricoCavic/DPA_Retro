73db7a4389dcc6f119845c9bcc2870ac1fc89f51
Enrico Cavicchioli <enricocavic@gmail.com>
2023-06-09 01:36:58 -0300

    implementação de tilha sonora multitrack

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -17,6 +17,7 @@ public class SoundManager : Singleton<SoundManager>
 
     private const float MINUTE = 60.0f;
     private double nextEventTime;
+    private int flipped;
 
     private List<AudioSource> soundtrackChannels;
 
@@ -29,29 +30,35 @@ public class SoundManager : Singleton<SoundManager>
         for (int i = 0; i < totalSoundtrackChannels; i++)
         {
             soundChannel = new GameObject("SoundChannel"+i);
-            soundtrackChannels.Add(soundChannel.AddComponent<AudioSource>());
             soundChannel.transform.parent = transform;
+            var audioSource = soundChannel.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false; 
+            soundtrackChannels.Add(audioSource);
         }
+
+        nextEventTime = AudioSettings.dspTime + 2 * preScheduleTime;
     }
 
     private void Update()
     {
-        double currentTime = AudioSettings.dspTime;
-
-        if (currentTime + preScheduleTime > nextEventTime)
+        if (AudioSettings.dspTime + preScheduleTime > nextEventTime)
         {
+            Debug.Log("Schedule next event");
             // lógica para escolher as próxima musica
             // possibilidade de existir uma fila de faixas populada dinamicamente
-            soundtrackChannels[0].clip = clipsTrack1[Random.Range(0, clipsTrack1.Count)];
-            soundtrackChannels[1].clip = clipsTrack2[Random.Range(0, clipsTrack2.Count)];
-            soundtrackChannels[2].clip = clipsTrack3[Random.Range(0, clipsTrack3.Count)];
+            // também será necessário fazer uma pool audio sources liberadas
+            soundtrackChannels[0 + flipped].clip = clipsTrack1[Random.Range(0, clipsTrack1.Count)];
+            soundtrackChannels[1 + flipped].clip = clipsTrack2[Random.Range(0, clipsTrack2.Count)];
+            soundtrackChannels[2 + flipped].clip = clipsTrack3[Random.Range(0, clipsTrack3.Count)];
 
-            soundtrackChannels[0].PlayScheduled(nextEventTime);
-            soundtrackChannels[1].PlayScheduled(nextEventTime);
-            soundtrackChannels[2].PlayScheduled(nextEventTime);
-            
+            soundtrackChannels[0 + flipped].PlayScheduled(nextEventTime);
+            soundtrackChannels[1 + flipped].PlayScheduled(nextEventTime);
+            soundtrackChannels[2 + flipped].PlayScheduled(nextEventTime);
+
 
             nextEventTime += MINUTE / bpm * numBeatsPerSegment;
+
+            flipped = 3 - flipped;
         }
     }
 }
