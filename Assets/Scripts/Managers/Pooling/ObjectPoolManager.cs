fab8cf7c7cab4a8d7119c0918b55a09a9540a762
Enrico Cavicchioli <enricocavic@gmail.com>
2023-06-10 00:11:06 -0300

    Update ObjectPoolManager.cs

--- Assets/Scripts/Managers/Pooling/ObjectPoolManager.cs
+++ Assets/Scripts/Managers/Pooling/ObjectPoolManager.cs
@@ -15,13 +15,13 @@ namespace Retro.Managers
             pools = new();
         }
 
-        public Pool AddNewPool(GameObject _prefabToPool, int _defaultCapacity = 10, int _maxSize = 1000)
+        public Pool AddNewPool(GameObject _prefabToPool, int _defaultCapacity = 30, int _maxSize = 1000)
         {
             if (pools.ContainsKey(_prefabToPool)) return pools[_prefabToPool];
 
-            GameObject obj = new GameObject("New Pool");
+            var obj = new GameObject($"{_prefabToPool.name} Pool");
             obj.transform.parent = transform;
-            Pool pool = obj.AddComponent<Pool>();
+            var pool = obj.AddComponent<Pool>();
             pool.prefab = _prefabToPool;
             pool.defaultCapacity = _defaultCapacity;
             pool.maxSize = _maxSize;
